3f66c5cd28f0bc6be5a48555fdd43daa47551f73
Sonicfangs <Sonicfangs>
2016-10-03 00:17:28 -0400

    util.js added

--- util.js
+++ util.js
@@ -1,37 +1,44 @@
 var MongoClient = require('mongodb').MongoClient;
 var Server = require('mongodb').Server;
-var url = 'mongodb://localhost:27017/match_history';
-var url2 = 'mongodb://localhost:27017/player_info';
-
+var url = 'mongodb://sonicfangs:password@ds019053.mlab.com:19053/smash_data';
+var matchList = {};
 
 var eloCalc = function(){
 MongoClient.connect(url,function(err, db){
-  MongoClient.connect(url2,function(err, db2){
 
   if(err)
     console.log(err);
   var col = db.collection("match_history");
-  var playerstuff = db2.collection("player_info");
     col.find({"week":{$exists:true}}).toArray(function(err, result){
         if(err)
           console.log(err);
           else{
             if(result != null){
-              var matchList = {
-              };
               for(var i = 0; i < result.length; i++){
               if(matchList[result[i].week] == null)
                   matchList[result[i].week] = [];
                 matchList[result[i].week].push(result[i]);
               }
               db.close();
-              db2.close();
               console.log(matchList);
             }
           }
       });
   });
+}
+
+var matchNew = function(matchJSON){
+  var winnerTemp = matchJSON.winner_id;
+  var loserTemp = matchJSON.loser_id;
+  console.log(winnerTemp + ":" + loserTemp);
+  MongoClient.connect(url, function(err, db){
+    if(err)
+      console.log(err);
+    var col = db.collection('match_history');
+    var player_info = db.collection('player_info');
+    db.close();
   });
 }
 
 eloCalc();
+matchNew({"winner_id":1, "loser_id":2});
